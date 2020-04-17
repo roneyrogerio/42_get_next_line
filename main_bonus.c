fdba29c7bab1b4b3dddf8aef3c1e1342d3812c3f
roneyrogerio <roneyrogerio92@gmail.com>
2020-04-17 08:22:29 -0300

    tests

--- main_bonus.c
+++ main_bonus.c
@@ -1,26 +1,28 @@
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 #include <stdio.h>
 #include <fcntl.h>
 #include <limits.h>
 
 int	main(void)
 {
-	int	fd;
+	int	fd, fd2;
 	char	*line;
 
 	fd = open("test", O_RDONLY);
+	fd2 = open("test2", O_RDONLY);
 	get_next_line(fd, &line);
 	printf("%s", line);
 	printf("\n");
-	get_next_line(fd, &line);
+	get_next_line(fd2, &line);
 	printf("%s", line);
 	printf("\n");
-	get_next_line(fd, &line);
+	get_next_line(fd2, &line);
 	printf("%s", line);
 	printf("\n");
 	get_next_line(fd, &line);
 	printf("%s", line);
 	printf("\n");
 	close(fd);
+	close(fd2);
 	return (0);
 }
